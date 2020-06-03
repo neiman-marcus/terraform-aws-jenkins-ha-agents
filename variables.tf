78e4d63a92c9f0326f47f5ff9c4b877a8dab912a
Clay Danford <clay_danford@neimanmarcus.com>
2020-06-03 00:19:47 -0500

    Bump jenkins and swarm version.

--- variables.tf
+++ variables.tf
@@ -108,7 +108,7 @@ variable "instance_type" {
 
 variable "jenkins_version" {
   description = "The version number of Jenkins to use on the master. Change this value when a new version comes out, and it will update the launch configuration and the autoscaling group."
-  default     = "2.222.3"
+  default     = "2.222.4"
 }
 
 variable "key_name" {
@@ -162,7 +162,7 @@ variable "ssm_parameter" {
 
 variable "swarm_version" {
   description = "The version of swarm plugin to install on the agents. Update by updating this value."
-  default     = "3.19"
+  default     = "3.20"
 }
 
 variable "tags" {
